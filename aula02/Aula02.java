0cf415f0eacfd8ad54e59d06f8959fbaf49429c6
andreluiz1902@gmail.com <andreluiz1902@gmail.com>
2022-03-28 15:01:32 -0300

    commit inicial

---
+++ aula02/Aula02.java
@@ -0,0 +1,13 @@
+public class Aula02 {
+  public static void main (String[] args) {
+    Caneta c1 = new Caneta();
+    c1.modelo = "Bic Crystal";
+    c1.cor = "Azul";
+    c1.ponta = 0.5f; //f no final representa float
+    c1.carga = 80;
+    c1.tampar();
+    
+    c1.status();
+    c1.rabiscar();
+  }
+}
